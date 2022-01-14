bcc30679f4a835d704de402840244d11cec6938e
procenkone <procenkon@gmail.com>
2022-01-14 14:30:31 +0200

    clean_master

--- clean-react-app/src/App.js
+++ clean-react-app/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Users from "./components/Users";
 
 function App() {
 
     return (
         <div className="App">
-            <Users/>
         </div>
     );
 }
--- clean-react-app/src/configs/urls.js
+++
@@ -1,7 +0,0 @@
-const baseUrl = 'https://jsonplaceholder.typicode.com'
-
-export const urls = {
-    users: `${baseUrl}/users`,
-    posts: `${baseUrl}/posts`,
-    comments: `${baseUrl}/comments`
-}
