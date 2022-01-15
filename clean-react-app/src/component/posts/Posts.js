9dc43189e209ef990e351bc8baa646fa7e3a9236
procenkone <procenkon@gmail.com>
2022-01-15 22:42:56 +0200

    temp post map

--- clean-react-app/src/component/posts/Posts.js
+++ clean-react-app/src/component/posts/Posts.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Posts = (posts) => {
-
+const Posts = ({posts}) => {
+    console.log(posts[0].id)
     return (
         <div>
-                        </div>
+            {/*{posts.map(item=>{item.id})}*/}
+
+        </div>
     );
 };
 
