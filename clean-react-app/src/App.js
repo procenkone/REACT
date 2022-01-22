5921466dca1152dd49150b00d8a30d54fca9bb2c
procenkone <procenkon@gmail.com>
2022-01-22 12:15:25 +0200

    react-router-dom

--- clean-react-app/src/App.js
+++ clean-react-app/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-
+import css from './App.module.css';
+import {Routes,Route} from 'react-router-dom'
 function App() {
 
     return (
-        <div className="App">
+        <div className={css.header}>
+            <a href={'/home'}>Home</a>
+            <a href={'/users'}>Users</a>
+            <a href={'/posts'}>Posts</a>
+            <a href={'/about'}>About</a>
         </div>
+
+        <Routes/>
     );
 }
 
