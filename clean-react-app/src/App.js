8e2a90e0e4372afe7baa51a0b34d892e91f30097
procenkone <procenkon@gmail.com>
2022-01-27 22:09:18 +0200

    CW_4 useReducer

--- clean-react-app/src/App.js
+++ clean-react-app/src/App.js
@@ -1,18 +1,87 @@
-import {Routes, Route} from 'react-router-dom'
+import {createRef, useReducer} from "react";
 
 import './App.css';
-import Layout from "./components/Layout/Layout";
+
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "addCat":
+            return [...state, {cat: action.cat, id: action.id}];
+
+        case "addDog":
+            return [...state, {dog: action.dog, id: action.id}]
+
+        case "delete":
+            return state.filter(item => item.id !== action.payload)
+
+        default:
+            return state
+    }
+}
+
 
 function App() {
+    const catRef = createRef()
+    const dogRef = createRef()
+    const [state, dispatch] = useReducer(reducer, [])
+
+
+    const addAnimal = (animal, id) => {
+        dispatch({
+            type: animal === 'cat' ? "addCat" : "addDog",
+            id: id,
+            [animal]: animal === 'cat' ? catRef.current.value : dogRef.current.value
+        })
+        animal === 'cat' ? catRef.current.value = '' : dogRef.current.value = ''
+    }
+    // const addCat = () => {
+    //     dispatch({
+    //         type: "addCat",
+    //         cat: catRef.current.value
+    //     })
+    //     catRef.current.value = ''
+    // }
+    //
+    // const addDog = () => {
+    //     dispatch({
+    //         type: "addDog",
+    //         dog: dogRef.current.value
+    //     })
+    //     dogRef.current.value = ''
+    // }
+
 
     return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<Layout/>}>
+        <div className='App'>
+            <div className={'inputWrap'}>
+                <div>
+                    <label>add cat:<input type={'text'} ref={catRef}/></label>
+                    <button onClick={() => addAnimal('cat', Date.now())}>addCat</button>
+                </div>
+                <div>
+                    <label>add dog:<input type={'text'} ref={dogRef}/></label>
+                    <button onClick={() => addAnimal('dog', Date.now())}>addDog</button>
+                </div>
+            </div>
+            <hr/>
 
+            <div className={'animalWrap'}>
+                <div>
+                    {state.map((cat) =>
+                        cat.hasOwnProperty('cat') && <div key={cat.id}>
+                            <h3>{cat.cat}</h3>
+                            <button onClick={() => dispatch({type: 'delete', payload: cat.id})}>delete</button>
+                        </div>)}
+                </div>
 
-                </Route>
-            </Routes>
+                <div>
+                    {state.map((dog) =>
+                        dog.hasOwnProperty('dog') && <div key={dog.id}>
+                            <h3>{dog.dog}</h3>
+                            <button onClick={() => dispatch({type: 'delete', payload: dog.id})}>delete</button>
+                        </div>)}
+                </div>
+            </div>
         </div>
     );
 }
--- clean-react-app/src/services/posts.services.js
+++
@@ -1,7 +0,0 @@
-import axiosServices from "./axios.services";
-import {urls} from "../configs/urls";
-
-export const postsServices = {
-    getAll:()=>axiosServices(urls.posts).then(value => value.data),
-    getById:(id)=>axiosServices(`${urls.posts}/${id}`).then(value => value.data)
-}
